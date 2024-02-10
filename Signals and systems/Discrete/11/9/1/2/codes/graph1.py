e0c2aa4cb35780a0b1dc5eb0a8abd0bf665c52ec
Praful-Kesavadas <ee23btech11049@iith.ac.in>
2024-02-10 10:31:52 +0530

    code modified

--- Signals and systems/Discrete/11/9/1/2/codes/graph1.py
+++ Signals and systems/Discrete/11/9/1/2/codes/graph1.py
@@ -1,19 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Read the sequence from the file
-with open("sequence.txt", "r") as file:
-    sequence = [float(line.strip()) for line in file]
+# Read the sequence from the file using numpy
+sequence_array = np.loadtxt("sequence.txt")
 
 # Plot the sequence
-sequence_array= np.array(sequence)
-
-# Add labels and title
 plt.stem(range(len(sequence_array)), sequence_array, basefmt="k-", use_line_collection=True)
 plt.xlabel('n')
 plt.ylabel('x(n)')
 plt.xticks(range(len(sequence_array)))
-#plt.title('Sequence Plot: n/(n+1)')
 
 # Save the graph as graph1.png
 plt.savefig('graph1.png')
